b7235fda1c3ba4a6ce423486685fd70e9d973cf8
erzhan <erzhan.sarsenov@gmail.com>
2025-05-02 18:28:06 +0500

    step #8.07

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
+set -e
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-#pip install -r requirements.txt
+echo "Скачиваем uv..."
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+. $HOME/.local/bin/env
 
+echo "Устанавливаем gunicorn..."
+pip3 install gunicorn
+
+echo "Синхронизируем зависимости..."
 make install
-# Convert static asset files
+
+echo "Компилируем переводы..."
+uv run python manage.py compilemessages
+make translate
+echo "Собираем статику..."
 make collectstatic
 
-# Apply any outstanding database migrations
+echo "Применяем миграции..."
 make migrate
+
+echo "Build script выполнен успешно."
